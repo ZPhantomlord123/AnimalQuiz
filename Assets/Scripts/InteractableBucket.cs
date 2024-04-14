5e70e731dc3a118cd5980617dc090678ea21f4be
Yash Naik <yashnaikgo@gmail.com>
2024-04-14 19:36:19 +0530

    Implemented Basic drag, drop, snap back and bucket interaction
    - Implemented scripts for cards and buckets.
    - Implemented logic for drag and drop and card snapping back.
    - Also created a template for bucket interaction.

--- Assets/Scripts/InteractableBucket.cs
+++ Assets/Scripts/InteractableBucket.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class InteractableBucket : MonoBehaviour
+public class InteractableBucket : MonoBehaviour, IDropHandler
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnDrop(PointerEventData eventData)
     {
-        InteractableCard card = collision.GetComponent<InteractableCard>();
+        InteractableCard card = eventData.pointerDrag.GetComponent<InteractableCard>();
+
         if (card != null)
         {
+            card.DisableCard();
             HandleDroppedCard(card);
         }
     }
 
     private void HandleDroppedCard(InteractableCard card)
     {
-        // Place logic here for handling the dropped card
-        Debug.Log("Card dropped into Bucket: " + gameObject.name);
+        // You can add additional logic here for handling the dropped card
+        Debug.Log("Card dropped into the bucket: " + card.gameObject.name);
     }
 }
--- Assets/Scripts/InteractableCard.cs
+++ Assets/Scripts/InteractableCard.cs
@@ -1,59 +1,54 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class InteractableCard : MonoBehaviour, IPointerDownHandler, IDragHandler
+public class InteractableCard : MonoBehaviour, IBeginDragHandler ,IEndDragHandler,IDragHandler
 {
-    private bool isDragging;
     private RectTransform rectTransform;
     private Canvas canvas;
     private CanvasGroup canvasGroup;
     private Vector2 offset;
+    private Vector2 originalPosition;
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
         canvasGroup = GetComponent<CanvasGroup>();
+        originalPosition = rectTransform.anchoredPosition;
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        isDragging = true;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out offset);
-        SetDraggedPosition(eventData);
+        canvasGroup.alpha = 0.6f;
         canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (isDragging)
-        {
-            SetDraggedPosition(eventData);
-        }
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        SnapBack();
     }
 
     private void SetDraggedPosition(PointerEventData eventData)
     {
-        if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
-        {
-            Vector2 localPointerPosition;
-            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, canvas.worldCamera, out localPointerPosition))
-            {
-                rectTransform.localPosition = localPointerPosition - offset;
-            }
-        }
-        else
-        {
-            Vector2 localPointerPosition;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, canvas.worldCamera, out localPointerPosition);
-            rectTransform.localPosition = localPointerPosition - offset;
-        }
+        Vector2 localPointerPosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, canvas.worldCamera, out localPointerPosition);
+        rectTransform.localPosition = localPointerPosition - offset;
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    public void DisableCard()
     {
-        isDragging = false;
-        canvasGroup.blocksRaycasts = true;
+        this.gameObject.SetActive(false);
+    }
+
+    public void SnapBack()
+    {
+        rectTransform.anchoredPosition = originalPosition;
     }
 }
